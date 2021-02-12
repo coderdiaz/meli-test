83cfb41720a215fd80c68a5deffa7d5856618a59
Javier Diaz <coderdiaz@gmail.com>
2021-02-11 21:09:47 -0600

    feat: added tailwindcss support

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ['./src/**/*.{tsx}'],
+  theme: {
+    colors: {
+      white: '#FFFFFF',
+      black: '#00000',
+      yellow: '#FFE600',
+      gray: {
+        200: '#EEEEEE',
+        300: '#999999',
+        400: '#666666',
+        500: '#333333',
+      },
+    },
+  },
+};
